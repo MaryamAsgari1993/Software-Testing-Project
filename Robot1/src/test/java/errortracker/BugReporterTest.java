85d7bb0b07156b8a1ee33cff34d0d23b8dae751c
Shriman Vinayagamoorthy <shriman30@hotmail.com>
2023-03-29 23:20:33 -0400

    Wrote some black box test for the robot's motion

--- Robot1/src/test/java/errortracker/BugReporterTest.java
+++ Robot1/src/test/java/errortracker/BugReporterTest.java
@@ -1,28 +1,28 @@
-// /**
-//  * Test class for testing the functionality of the BugReporter method.
-//  * @author Maryam Asgari and Shima Omidvar 
-//  * @since 2023-02-04
-//  */
-// package errortracker;
+/**
+ * Test class for testing the functionality of the BugReporter method.
+ * @author Maryam Asgari and Shima Omidvar 
+ * @since 2023-02-04
+ */
+package errortracker;
 
-// import junit.framework.TestCase;
+import junit.framework.TestCase;
 
-// import org.junit.Test;
+import org.junit.Test;
 
-// public class BugReporterTest extends TestCase{
+public class BugReporterTest extends TestCase{
 	
-// 	/**
-//      * Test case for verifying the BugReporter functionality.
-//      * 
-//      * @param an instance error message. in this case, This is a bugs is the error message. 
-//      * @throws Exception if any error occurs during the program. In this case, we expected to have a This is a bug as an exception.
-//      */
-// 	@Test
-//     public void testBugReporter() {
-//         String expectedMessage = "This is a bug";
-//         BugReporter bugReporter = new BugReporter(expectedMessage);
-//         String actualMessage = bugReporter.getMessage();
-//         assertEquals(expectedMessage, actualMessage);
-//     }
+	/**
+     * Test case for verifying the BugReporter functionality.
+     * 
+     * @param an instance error message. in this case, This is a bugs is the error message. 
+     * @throws Exception if any error occurs during the program. In this case, we expected to have a This is a bug as an exception.
+     */
+	@Test
+    public void testBugReporter() {
+        String expectedMessage = "This is a bug";
+        BugReporter bugReporter = new BugReporter(expectedMessage);
+        String actualMessage = bugReporter.getMessage();
+        assertEquals(expectedMessage, actualMessage);
+    }
 
-// }
+}
